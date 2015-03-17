97abdf2282db8ccdfa05aa8f8066677268a41cd7
Marcelo Augusto <marcelothis@gmail.com>
2015-03-17 15:06:24 -0300

    Alteração Command
    Alteração padrão command

---
+++ src/br/edu/ifrn/diatinf/cubo/entities/RotacoesCommand.java
@@ -0,0 +1,14 @@
+package br.edu.ifrn.diatinf.cubo.entities;
+
+public class CuboMagico implements InterfaceCommand{
+
+	@Override
+	public void rotacionar(Face facePrincipal) {
+				
+		Face face = new Face();
+		
+		face = facePrincipal;
+			
+	}
+	
+}
